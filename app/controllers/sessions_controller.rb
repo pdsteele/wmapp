0687a2258c8638a599419afe24025593eb0d7fd3
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-02 16:53:45 -0500

    wrote sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      self.current_user = user
+      flash[:success] = "You are now logged in."
+      redirect_to root_url
+    else
+      flash.now[:error] = "Couldn't find a user with those credentials."
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    self.current_user = nil
+    flash[:success] = "You have been signed out."
+    redirect_to root_url
+  end
 end
