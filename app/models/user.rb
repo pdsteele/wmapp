c031d6658a1d1175ad2b31d5d3ade832c67ff05b
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-02 17:31:05 -0500

    updated accessible attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   include SimplestAuth::Model
 
+  attr_accessible :email, :password, :password_confirmation
+  attr_accessor   :password
+
   validates :email, :presence => true,
                     :uniqueness => { :case_sensitive => false }
   validates :password, :confirmation => true
