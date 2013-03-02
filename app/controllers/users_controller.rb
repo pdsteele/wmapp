ca090ce33185be26f25d6280e0cabf7068571804
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-02 16:50:26 -0500

    wrote some methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Your account has been successfully created!"
+      self.current_user = @user
+      redirect_to @user
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def password
+  end
+
+  def update
+    if current_user.update_attributes(params[:user])
+      flash[:success] = "Your password has been changed."
+      redirect_to root_url
+    else
+      render "password"
+    end
+  end
 end
