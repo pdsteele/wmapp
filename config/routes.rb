4de53b9e87ad56ade83bc0a56fd9c296507e373c
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-02 17:22:15 -0500

    added a root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Wmapps::Application.routes.draw do
   resources :users, :execpt => [:destroy, :edit]
   resources :sessions, :only => [:new, :create, :destroy]
 
+  root :to => 'pages#home'
+
   match '/login',   :to => 'sessions#new'
   match '/logout',  :to => 'sessions#destroy', via: :delete
   match '/signup',  :to => 'users#new'
