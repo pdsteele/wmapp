f2dba658ec176803289c91fc63d978825e0a2f04
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-03 09:37:07 -0500

    wrote user request spec for signing up

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Users" do
+  let!(:user) { FactoryGirl.build(:user) }
+
+  describe "when not logged in" do
+    it "can sign up with valid info" do
+      visit '/signup'
+
+      fill_in 'Email', :with => 'jake@email.wm.edu'
+      fill_in 'user_password', :with => 'foobar'
+      fill_in 'user_password_confirmation', :with => 'foobar'
+
+      click_button 'Create Account'
+
+      page.should have_content 'Your account has been successfully created!'
+    end
+  end
+end
