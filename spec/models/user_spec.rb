1e5bb7728a84340cd27acb0f50f5de30a8af6d53
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-03 10:05:22 -0500

    wrote specs to check valid email format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,18 @@ describe User do
         it { should validate_uniqueness_of(:email) }
         it { should validate_confirmation_of(:password) }
         it { should validate_presence_of(:password_confirmation) }
+
+        context "when email format is valid" do
+            valid_emails.each do |email|
+                it { should allow_value(email).for(:email) }
+            end
+        end
+
+        context "when email format is invalid" do
+            invalid_emails.each do |email|
+                it { should_not allow_value(email).for(:email) }
+            end
+        end
     end
     
 end
