1af109dadbe76ea019d61523799236a4375bdc2c
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-03 09:40:22 -0500

    wrote test for bad signup info

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,5 +15,16 @@ describe "Users" do
 
       page.should have_content 'Your account has been successfully created!'
     end
+
+    it "cannot sign up with bad info" do
+      visit '/signup'
+
+      fill_in 'Email', :with => 'jake@email.wm.edu'
+      fill_in 'user_password', :with => 'foobar'
+
+      click_button 'Create Account'
+
+      page.should have_content '2 errors prohibited your account from being created:'
+    end
   end
 end
