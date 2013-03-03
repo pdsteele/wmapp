47b414512d5a20a2da131a87ba508c3f165c8a4c
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-03 10:05:33 -0500

    added valid & invalid email helpers

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -4,3 +4,11 @@ def log_in
   fill_in 'Password', :with => 'dailyshow'
   click_button 'Log in'
 end
+
+def valid_emails
+  %w[jake@email.wm.edu jonstewart@email.wm.edu tj@email.wm.edu]
+end
+
+def invalid_emails
+  %w[jake@gmail.com willsmith oldie@wm.edu]
+end
