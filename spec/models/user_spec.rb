2aa60754b797f3ade149fc42657a1b6ce693da64
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-13 09:41:55 -0400

    added tests for grad year and dorm

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,14 @@ describe User do
         it { should validate_uniqueness_of(:email) }
         it { should validate_confirmation_of(:password) }
         it { should validate_presence_of(:password_confirmation) }
+        it { should validate_presence_of(:dorm) }
+        it { should validate_presence_of(:grad_year) }
+
+        describe "graduating year" do
+          it "should be a valid year" do
+            (2013..2016).to_a.should include(:grad_year)
+          end
+        end
 
         context "when email format is valid" do
             valid_emails.each do |email|
