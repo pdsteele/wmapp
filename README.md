0f77a0e8e2a115066c456c285e6ed12e10eccbc1
Philip Steele <pdsteele@wm.edu>
2013-03-28 17:23:09 -0400

    added building generating script in case DB gets wiped

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ William & Mary Maintenance Request App
 ======================================
 
 This is the github repository for our Software Engineering class project.
+
+To get this working, just clone the repo and run bundle install --without production 
+Then rails s will load the site 
+
+To get the database populated with buildings (in case it gets wiped or anything like that), there is a script in /scripts called addBuildings script. Load the console with "rails c" and then run the command "load("scripts/addBuildingScript.rb")"
+
---
+++ addBuildingScript.rb
@@ -0,0 +1,88 @@
+Buildings.new("Adair").save
+Buildings.new("Andrews").save
+Buildings.new("Blow").save
+Buildings.new("Bozarth").save
+Buildings.new("Center for Archaeological Research").save
+Buildings.new("Ceramics Studio").save
+Buildings.new("Endocrinology Lab").save
+Buildings.new("Ewell").save
+Buildings.new("ISC1").save
+Buildings.new("ISC2").save
+Buildings.new("James Blair ").save
+Buildings.new("Jones").save
+Buildings.new("Keck").save
+Buildings.new("Law School").save
+Buildings.new("Matoaka Art Studio").save
+Buildings.new("McGlothlin-Street Hall").save
+Buildings.new("Miller").save
+Buildings.new("Millington").save
+Buildings.new("Morton").save
+Buildings.new("PBK Hall").save
+Buildings.new("Prince George House").save
+Buildings.new("Small").save
+Buildings.new("School Of Education").save
+Buildings.new("Tucker").save
+Buildings.new("Tyler").save
+Buildings.new("Washington").save
+Buildings.new("Wren").save
+Buildings.new("Barrett").save
+Buildings.new("Brown").save
+Buildings.new("Bryan").save
+Buildings.new("Cabell").save
+Buildings.new("Camm").save
+Buildings.new("Campus Center").save
+Buildings.new("Chandler ").save
+Buildings.new("Commons Dining Hall").save
+Buildings.new("Dawson").save
+Buildings.new("Dinwiddie").save
+Buildings.new("Dupont").save
+Buildings.new("Fauquier").save
+Buildings.new("Giles").save
+Buildings.new("Gooch").save
+Buildings.new("Graduate Housing Complex").save
+Buildings.new("Harrison").save
+Buildings.new("Hughes").save
+Buildings.new("Hunt").save
+Buildings.new("Jamestown North").save
+Buildings.new("Jamestown South").save
+Buildings.new("Jefferson ").save
+Buildings.new("Landrum").save
+Buildings.new("Lodge 2").save
+Buildings.new("Lodge 4").save
+Buildings.new("Lodge 6").save
+Buildings.new("Lodge 8").save
+Buildings.new("Lodge 10").save
+Buildings.new("Lodge 12").save
+Buildings.new("Lodge 16").save
+Buildings.new("Ludwell Apts").save
+Buildings.new("Madison").save
+Buildings.new("Meridian Coffee House").save
+Buildings.new("Monroe ").save
+Buildings.new("Munford").save
+Buildings.new("Nicholas").save
+Buildings.new("Nicholson").save
+Buildings.new("Old Dominion ").save
+Buildings.new("Page").save
+Buildings.new("Pleasants").save
+Buildings.new("Preston").save
+Buildings.new("Reves Hall").save
+Buildings.new("Sadler Center ").save
+Buildings.new("Sor 1: KKG").save
+Buildings.new("Sor 2: ACO").save
+Buildings.new("Sor 3: CO").save
+Buildings.new("Sor 4: DDD").save
+Buildings.new("Sor 5: PBP").save
+Buildings.new("Sor 6: KD").save
+Buildings.new("Sor 7: DG").save
+Buildings.new("Sor 8: PM").save
+Buildings.new("Sor 9: KAT").save
+Buildings.new("Sor 10: Bozzarth").save
+Buildings.new("Sor 11: GPB").save
+Buildings.new("Sor 12: DP(Mullen)").save
+Buildings.new("Spotswood").save
+Buildings.new("Stith").save
+Buildings.new("Taliaferro").save
+Buildings.new("Tazewell").save
+Buildings.new("Tribe Square").save
+Buildings.new("Units (specify letter in room number)").save
+Buildings.new("Yates").save
