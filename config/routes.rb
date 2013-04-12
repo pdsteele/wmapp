41c8dc85bff9acbc0bb6278e5432ca695e07c57b
Philip Steele <pdsteele@wm.edu>
2013-04-12 01:16:49 -0400

    worker workorder pages implemented

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Wmapps::Application.routes.draw do
   match '/login',   :to => 'sessions#new'
   match '/logout',  :to => 'sessions#destroy', via: :delete
   match '/signup',  :to => 'users#new'
+  match '/assigned_workorders', :to => 'worker#show_assigned'
+  match '/progress_workorders', :to => 'worker#show_in_progress'
+  #match '/clients/:status' => 'clients#index', :foo => "bar" 
 end
 
 
