9b24b64b0f067a8532411a72e06e9fa230dc2533
Philip Steele <pdsteele@wm.edu>
2013-05-05 20:15:46 -0400

    removed migration that breaks db migrate for all that didn't have old version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503171313) do
+ActiveRecord::Schema.define(:version => 20130503054953) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
